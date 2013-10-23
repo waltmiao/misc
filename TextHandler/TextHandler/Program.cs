ecfa3e355a17d91febb4585af15c74f95bf3c13e
Walt Miao <wei.miao@gmail.com>
2013-10-23 17:26:29 +0800

    update

--- TextHandler/TextHandler/Program.cs
+++ TextHandler/TextHandler/Program.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 using System.IO;
+using System.Security;
+using System.Security.Cryptography;
+using System.Runtime.InteropServices;
 
 namespace TextHandler
 {
@@ -9,9 +12,101 @@ namespace TextHandler
     {
         static void Main(string[] args)
         {
-            string path = @"D:\data\112\20130719";
+             //string pathParent = @"C:\tasks\100";
+             //DirectoryInfo dir = new DirectoryInfo(pathParent);
+             //foreach (DirectoryInfo child in dir.GetDirectories("*"))
+             //{
+             //    HandleFolder(child.FullName, pathParent);
+             //}
+
+            string key = GenerateKey();//"waltmiao";
+
+            //EncryptFile(@"c:\tasks\100.zip", @"c:\tasks\200.dat", key);
+            //DecryptFile(@"c:\tasks\200.dat", @"c:\tasks\100_1.zip", key);
+
+            GCHandle gch = GCHandle.Alloc(key, GCHandleType.Pinned);
+
+            EncryptFile(@"c:\tasks\100.csv", @"c:\tasks\200.zzz", key);
+            DecryptFile(@"c:\tasks\200.zzz", @"c:\tasks\100_1.csv", key);
+
+
+            gch.Free();
+
+
+        }
+        static string GenerateKey()
+        {
+            // Create an instance of Symetric Algorithm. Key and IV is generated automatically.
+            DESCryptoServiceProvider desCrypto = (DESCryptoServiceProvider)DESCryptoServiceProvider.Create();
+
+            // Use the Automatically generated key for Encryption. 
+            return ASCIIEncoding.ASCII.GetString(desCrypto.Key);
+        }
+
+
+        static void EncryptFile(string sInputFilename,
+         string sOutputFilename,
+         string sKey)
+        {
+            FileStream fsInput = new FileStream(sInputFilename,
+               FileMode.Open,
+               FileAccess.Read);
+
+            FileStream fsEncrypted = new FileStream(sOutputFilename,
+               FileMode.Create,
+               FileAccess.Write);
+            DESCryptoServiceProvider DES = new DESCryptoServiceProvider();
+            DES.Key = ASCIIEncoding.ASCII.GetBytes(sKey);
+            DES.IV = ASCIIEncoding.ASCII.GetBytes(sKey);
+            ICryptoTransform desencrypt = DES.CreateEncryptor();
+            CryptoStream cryptostream = new CryptoStream(fsEncrypted,
+               desencrypt,
+               CryptoStreamMode.Write);
+
+            byte[] bytearrayinput = new byte[fsInput.Length];
+            fsInput.Read(bytearrayinput, 0, bytearrayinput.Length);
+            cryptostream.Write(bytearrayinput, 0, bytearrayinput.Length);
+            cryptostream.Close();
+            fsInput.Close();
+            fsEncrypted.Close();
+        }
+
+        static void DecryptFile(string sInputFilename,
+           string sOutputFilename,
+           string sKey)
+        {
+            DESCryptoServiceProvider DES = new DESCryptoServiceProvider();
+            //A 64 bit key and IV is required for this provider.
+            //Set secret key For DES algorithm.
+            DES.Key = ASCIIEncoding.ASCII.GetBytes(sKey);
+            //Set initialization vector.
+            DES.IV = ASCIIEncoding.ASCII.GetBytes(sKey);
+
+            //Create a file stream to read the encrypted file back.
+            FileStream fsread = new FileStream(sInputFilename,
+               FileMode.Open,
+               FileAccess.Read);
+            //Create a DES decryptor from the DES instance.
+            ICryptoTransform desdecrypt = DES.CreateDecryptor();
+            //Create crypto stream set to read and do a 
+            //DES decryption transform on incoming bytes.
+            CryptoStream cryptostreamDecr = new CryptoStream(fsread,
+               desdecrypt,
+               CryptoStreamMode.Read);
+            //Print the contents of the decrypted file.
+            StreamWriter fsDecrypted = new StreamWriter(sOutputFilename);
+            fsDecrypted.Write(new StreamReader(cryptostreamDecr).ReadToEnd());
+            fsDecrypted.Flush();
+            fsDecrypted.Close();
+        } 
+
+
+
+        private static void HandleFolder(string pathChild, string pathParent)
+        {
+           // string path = @"C:\tasks\100\20130401";
             Dictionary<string, string> money = new Dictionary<string, string>();
-            using (StreamReader sr = new StreamReader(Path.Combine(path, "bcp_file_cust_real_fund.txt"), Encoding.Default))
+            using (StreamReader sr = new StreamReader(Path.Combine(pathChild, "bcp_file_cust_real_fund.txt"), Encoding.Default))
             {
                 string line;
                 while ((line = sr.ReadLine()) != null)
@@ -28,7 +123,7 @@ namespace TextHandler
 
             string name = "clients.csv";
 
-            using (StreamReader sr = new StreamReader(Path.Combine(path, "bcp_file_dictionary.txt"), Encoding.Default))
+            using (StreamReader sr = new StreamReader(Path.Combine(pathChild, "bcp_file_dictionary.txt"), Encoding.Default))
             {
                 string line;
                 while ((line = sr.ReadLine()) != null)
@@ -36,7 +131,7 @@ namespace TextHandler
                     line = line.Trim('|');
                     string[] words = line.Split(new string[] { "^@#~" }, StringSplitOptions.RemoveEmptyEntries);
                     if (words.Length > 2 && words[0] == "1000" && words[1] == "Name")
-                        name = words[2] + path.Substring(path.Length - 8) + ".csv";
+                        name = words[2] + pathChild.Substring(pathChild.Length - 8) + ".csv";
                 }
 
             }
@@ -44,9 +139,9 @@ namespace TextHandler
             List<string> ss = new List<string>();
             int iii = 0;
 
-            using (StreamReader sr = new StreamReader(Path.Combine(path, "bcp_file_cust_whole.txt"), Encoding.Default))
+            using (StreamReader sr = new StreamReader(Path.Combine(pathChild, "bcp_file_cust_whole.txt"), Encoding.Default))
             {
-                using (StreamWriter sw = new StreamWriter(Path.Combine(path, name), false, Encoding.Default))
+                using (StreamWriter sw = new StreamWriter(Path.Combine(pathParent, name), false, Encoding.Default))
                 {
                     sw.WriteLine("姓名,出生年份,手机号,资金,地址,身份证号");
                     string line;
