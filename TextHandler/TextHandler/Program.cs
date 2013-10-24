ac7b9bb7de21ba8c2500cd21c1591c6c95ef25c4
Walt Miao <wei.miao@gmail.com>
2013-10-24 10:05:03 +0800

    for csv

--- TextHandler/TextHandler/Program.cs
+++ TextHandler/TextHandler/Program.cs
@@ -4,7 +4,6 @@ using System.Text;
 using System.IO;
 using System.Security;
 using System.Security.Cryptography;
-using System.Runtime.InteropServices;
 
 namespace TextHandler
 {
@@ -12,95 +11,14 @@ namespace TextHandler
     {
         static void Main(string[] args)
         {
-             //string pathParent = @"C:\tasks\100";
-             //DirectoryInfo dir = new DirectoryInfo(pathParent);
+             string pathParent = @"C:\tasks\100";
+             DirectoryInfo dir = new DirectoryInfo(pathParent);
              //foreach (DirectoryInfo child in dir.GetDirectories("*"))
              //{
              //    HandleFolder(child.FullName, pathParent);
              //}
 
-            string key = GenerateKey();//"waltmiao";
-
-            //EncryptFile(@"c:\tasks\100.zip", @"c:\tasks\200.dat", key);
-            //DecryptFile(@"c:\tasks\200.dat", @"c:\tasks\100_1.zip", key);
-
-            GCHandle gch = GCHandle.Alloc(key, GCHandleType.Pinned);
-
-            EncryptFile(@"c:\tasks\100.csv", @"c:\tasks\200.zzz", key);
-            DecryptFile(@"c:\tasks\200.zzz", @"c:\tasks\100_1.csv", key);
-
-
-            gch.Free();
-
-
         }
-        static string GenerateKey()
-        {
-            // Create an instance of Symetric Algorithm. Key and IV is generated automatically.
-            DESCryptoServiceProvider desCrypto = (DESCryptoServiceProvider)DESCryptoServiceProvider.Create();
-
-            // Use the Automatically generated key for Encryption. 
-            return ASCIIEncoding.ASCII.GetString(desCrypto.Key);
-        }
-
-
-        static void EncryptFile(string sInputFilename,
-         string sOutputFilename,
-         string sKey)
-        {
-            FileStream fsInput = new FileStream(sInputFilename,
-               FileMode.Open,
-               FileAccess.Read);
-
-            FileStream fsEncrypted = new FileStream(sOutputFilename,
-               FileMode.Create,
-               FileAccess.Write);
-            DESCryptoServiceProvider DES = new DESCryptoServiceProvider();
-            DES.Key = ASCIIEncoding.ASCII.GetBytes(sKey);
-            DES.IV = ASCIIEncoding.ASCII.GetBytes(sKey);
-            ICryptoTransform desencrypt = DES.CreateEncryptor();
-            CryptoStream cryptostream = new CryptoStream(fsEncrypted,
-               desencrypt,
-               CryptoStreamMode.Write);
-
-            byte[] bytearrayinput = new byte[fsInput.Length];
-            fsInput.Read(bytearrayinput, 0, bytearrayinput.Length);
-            cryptostream.Write(bytearrayinput, 0, bytearrayinput.Length);
-            cryptostream.Close();
-            fsInput.Close();
-            fsEncrypted.Close();
-        }
-
-        static void DecryptFile(string sInputFilename,
-           string sOutputFilename,
-           string sKey)
-        {
-            DESCryptoServiceProvider DES = new DESCryptoServiceProvider();
-            //A 64 bit key and IV is required for this provider.
-            //Set secret key For DES algorithm.
-            DES.Key = ASCIIEncoding.ASCII.GetBytes(sKey);
-            //Set initialization vector.
-            DES.IV = ASCIIEncoding.ASCII.GetBytes(sKey);
-
-            //Create a file stream to read the encrypted file back.
-            FileStream fsread = new FileStream(sInputFilename,
-               FileMode.Open,
-               FileAccess.Read);
-            //Create a DES decryptor from the DES instance.
-            ICryptoTransform desdecrypt = DES.CreateDecryptor();
-            //Create crypto stream set to read and do a 
-            //DES decryption transform on incoming bytes.
-            CryptoStream cryptostreamDecr = new CryptoStream(fsread,
-               desdecrypt,
-               CryptoStreamMode.Read);
-            //Print the contents of the decrypted file.
-            StreamWriter fsDecrypted = new StreamWriter(sOutputFilename);
-            fsDecrypted.Write(new StreamReader(cryptostreamDecr).ReadToEnd());
-            fsDecrypted.Flush();
-            fsDecrypted.Close();
-        } 
-
-
 
         private static void HandleFolder(string pathChild, string pathParent)
         {
